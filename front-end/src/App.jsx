1d6bfd9493063ea066b1d914387f9c4b9d306ae3
Joshua Bastien <joshuabastienps@gmail.com>
2023-10-14 23:56:28 -0400

    Created wip homepage

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   <>
     {<BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/webcam" element={<Conversation />} />
         </Routes>
       </BrowserRouter>}
--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,13 +1,19 @@
-import '../App.css'
-import Character from '../components/character'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Home() {
+const Home = () => {
   return (
-    <>
-      <Character/>
-    </>
-  )
-}
+    <div className="home-container flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Welcome to FakeIt</h1>
+      <p className="text-lg mb-8">
+        Engage with our friendly AI! Just turn on your webcam and microphone, and our AI
+        will analyze your expressions, encouraging you with a little chat.
+      </p>
+      <Link to="/choice" className="explore-button bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600">
+        Start Interacting
+      </Link>
+    </div>
+  );
+};
 
-export default Home
-
+export default Home;
