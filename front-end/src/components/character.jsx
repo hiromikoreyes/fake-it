25c6c4c5d56c09fe51c03b847d060136259ef143
Joshua Bastien <joshuabastienps@gmail.com>
2023-10-15 06:49:57 -0400

    Added character images

--- front-end/src/components/character.jsx
+++ front-end/src/components/character.jsx
@@ -21,7 +21,7 @@ export default function Character(){
                     REX
                 </div>
                 <div className="cc-img-container m24">
-                    <img src="https://img.freepik.com/premium-vector/cartoon-t-rex-was-standing-with-two-legs_9633-4.jpg?w=996"/>
+                    <img src="/rex.jpg"/>
                 </div>
                 <div className="cc-body text-center m-10">
                     Rex is your best friend who feeds into all your wacky and crazy delusions. 
@@ -32,7 +32,7 @@ export default function Character(){
                     MOM
                 </div>
                 <div className="cc-img-container m24 text-center">
-                    <img src="https://img.freepik.com/premium-vector/cartoon-t-rex-was-standing-with-two-legs_9633-4.jpg?w=996"/>
+                    <img src="/mom.jpg"/>
                 </div>
                 <div className="cc-body m-10 text-center">
                     Your loving, caring, overendearing, warm, mother.
@@ -43,7 +43,7 @@ export default function Character(){
                     ALEX
                 </div>
                 <div className="cc-img-container m24">
-                    <img src="https://img.freepik.com/premium-vector/cartoon-t-rex-was-standing-with-two-legs_9633-4.jpg?w=996"/>
+                    <img src="/alex.jpg "/>
                 </div>
                 <div className="cc-body m-10">
                     Your biggest fan.
