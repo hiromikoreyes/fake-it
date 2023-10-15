ada9ce7f737777da2d271127f23e81c380abdb48
AlvinCao88 <alvincao88@gmail.com>
2023-10-14 23:20:29 -0400

    Graph with image

--- back-end/app.py
+++ back-end/app.py
@@ -1,24 +1,24 @@
-# from flask import Flask, jsonify, request
-# from flask_cors import CORS, cross_origin
-# import generation
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+import generation
 import matplotlib.pyplot as plt
 import numpy as np
 
-# app = Flask(__name__)
-# CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# @app.route('/test', methods=["GET"])
-# def test():
-#     return {"i love casting spells": 200}
+@app.route('/test', methods=["GET"])
+def test():
+    return {"i love casting spells": 200}
 
-# @app.route('/generate', methods=["POST"])
-# def generate():
-#     data = request.get_json()
-#     result = generation.response(data["text"])
-#     return jsonify({'response': result})
+@app.route('/generate', methods=["POST"])
+def generate():
+    data = request.get_json()
+    result = generation.response(data["text"])
+    return jsonify({'response': result})
 
-# @app.route('/create-graph', methods=["POST"])
+@app.route('/create-graph', methods=["POST"])
 def creategraph(list):
     eval_x = []
     score_y = []
