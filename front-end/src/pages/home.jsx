f40ec09fad7b610ee2b3680bda91babbd33204cf
AlvinCao88 <alvincao88@gmail.com>
2023-10-14 22:27:59 -0400

    plotting and values

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,12 +1,10 @@
 import '../App.css'
 import Character from '../components/character'
-import Webcam, { getCurrentMood } from '../components/webcam';
 
 function Home() {
   return (
     <>
       <Character/>
-      <Webcam/>
     </>
   )
 }
