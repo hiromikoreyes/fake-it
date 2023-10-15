5621a93c5f8f4c3ce53bbe1c6eba0521a61d4aaa
AlvinCao88 <alvincao88@gmail.com>
2023-10-14 22:43:36 -0400

    Graph with image

--- back-end/app.py
+++ back-end/app.py
@@ -1,36 +1,50 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-import generation
+# from flask import Flask, jsonify, request
+# from flask_cors import CORS, cross_origin
+# import generation
 import matplotlib.pyplot as plt
 import numpy as np
 
-app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+# app = Flask(__name__)
+# CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/test', methods=["GET"])
-def test():
-    return {"i love casting spells": 200}
+# @app.route('/test', methods=["GET"])
+# def test():
+#     return {"i love casting spells": 200}
 
-@app.route('/generate', methods=["POST"])
-def generate():
-    data = request.get_json()
-    result = generation.response(data["text"])
-    return jsonify({'response': result})
+# @app.route('/generate', methods=["POST"])
+# def generate():
+#     data = request.get_json()
+#     result = generation.response(data["text"])
+#     return jsonify({'response': result})
 
-@app.route('/create-graph', methods=["POST"])
+# @app.route('/create-graph', methods=["POST"])
 def creategraph(list):
-    x = []
-    y = [                                                                                                                                                                                                                                                                                               ]
-    for item in list:
-        x.append(item["num_eval"])
-        y.append(item["score"])
-    # eval_x = np.array(eval_x)
-    # score_y = np.array(score_y)
+    eval_x = []
+    score_y = []
+    
+    for item in data_list:
+        eval_x.append(item["num_eval"])
+        score_y.append(item["score"])
+    
+    eval_x = np.array(eval_x)
+    score_y = np.array(score_y)
 
-plt.plot(x, y)
-plt.show()
-                      
+    plt.plot(eval_x, score_y)
+    plt.xlabel("Time passed")  
+    plt.ylabel("Confidence level")  
+    # plt.title("Evaluation vs. Score")  # Add a title
+    plt.show()
+
+    # buffer = io.BytesIO()
+    # plt.savefig(buffer, format='png')
+    # buffer.seek(0)
+
+    # # Clear the figure to release memory
+    # plt.clf()
+
+    # # Return the image as a response
+    # return send_file(buffer, mimetype='image/png')
 
     
 
