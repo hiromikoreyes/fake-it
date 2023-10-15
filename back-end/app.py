c2c561d624e958e14bc299fbf671f990e2e9907f
AlvinCao88 <alvincao88@gmail.com>
2023-10-14 22:26:41 -0400

    plotting and values

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import generation
+import matplotlib.pyplot as plt
+import numpy as np
+
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -14,3 +17,20 @@ def generate():
     data = request.get_json()
     result = generation.response(data["text"], data["persona"], data["mood"])
     return jsonify({'response': result})
+
+@app.route('/create-graph', methods=["POST"])
+def creategraph(list):
+    x = []
+    y = [                                                                                                                                                                                                                                                                                               ]
+    for item in list:
+        x.append(item["num_eval"])
+        y.append(item["score"])
+    # eval_x = np.array(eval_x)
+    # score_y = np.array(score_y)
+
+plt.plot(x, y)
+plt.show()
+                      
+
+    
+
